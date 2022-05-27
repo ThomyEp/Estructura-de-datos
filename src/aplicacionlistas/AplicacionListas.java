d6323e9ac45e25683d1530e949c471302ad63861
alumnos <alumnos@UIOOCC106-C-19.alumnosuio.ute.edu.ec>
2022-05-27 10:52:17 -0500

    Listas_enlazadas_simples

---
+++ src/aplicacionlistas/AplicacionListas.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplicacionlistas;
+
+/**
+ *
+ * @author alumnos
+ */
+public class AplicacionListas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       //Lista enlazada simple 
+       
+    }
+    
+}
---
+++ src/aplicacionlistas/Lista.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aplicacionlistas;
+
+/**
+ *
+ * @author alumnos
+ */
+public class Lista {
+    class Nodo {
+        int info;
+        Nodo sig;
+    }
+    private Nodo raiz;
+    
+    public Lista (){ //Constructor
+        raiz=null;
+        
+    }
+    public int cantidadNodos(){
+        int cantidad=0;
+        Nodo recorrido=raiz;
+        while (recorrido != null){
+            recorrido=recorrido.sig;
+            cantidad++;
+            
+        }
+        return cantidad;
+    }
+    public void insertar (int pos, int informacion){
+        if (pos<=cantidadNodos()+1){
+            Nodo nuevo=new Nodo();
+            nuevo.info=informacion;
+            if (pos==1){
+                nuevo.sig=raiz;
+                raiz=nuevo;
+            }else {
+                if(pos==cantidadNodos()+1){
+                    Nodo recorrido=raiz;
+                    while(recorrido!=null){
+                        recorrido=recorrido.sig;
+                    }
+                    recorrido.sig=nuevo;
+                    nuevo.sig=null;
+                    
+                }else{
+                   
+                 Nodo recorrido= raiz;
+                    for (int i = 0; i < pos-1; i++) {
+                        recorrido=recorrido.sig;     
+                    }
+                    Nodo siguiente=recorrido.sig;
+                    recorrido.sig=nuevo;
+                    nuevo.sig=siguiente;
+                }
+            }  
+         }      
+     }
+}
