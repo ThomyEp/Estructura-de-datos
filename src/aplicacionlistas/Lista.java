7b84e162165954d8f9c7627a28966651456b5872
ThomyEp <104998274+ThomyEp@users.noreply.github.com>
2022-06-02 11:01:45 -0500

    Update Lista.java

--- src/aplicacionlistas/Lista.java
+++ src/aplicacionlistas/Lista.java
@@ -73,4 +73,84 @@ public class Lista {
             return false;
         }
     }
+    public boolean buscarElemento(int elemento){
+        Nodo recorrido=raiz;
+        while(recorrido!=null){
+            if (recorrido.info==elemento) {
+                     return true;
+        }
+            recorrido=recorrido.sig;
+    }
+       return false;
+    }
+    //mayor elemento y su posicion
+    public void mayorElemento(){
+        int mayor=0;
+        int posicion=1;
+        Nodo recorrido=raiz;
+        if (!vaciaLista()) {
+            while (recorrido!=null) {  
+                if (recorrido.info>mayor) {
+                     mayor=recorrido.info;//elemento mayor
+                   posicion=posicion+1; //Posicion del elemento
+                }
+               recorrido=recorrido.sig;
+            }
+             System.out.println("el mayor elemento es:"+mayor+" y su posicion es: "+(posicion-1));
+         } 
+        else{
+            System.out.println("No existe elementos en la lista");
+        }
+    }
+    public void ordenarLista(){
+        Nodo recorrido=raiz;
+        Nodo recorrido1=raiz;
+        if (!vaciaLista()) {
+            for (int i=1;i<recorrido.info;i++){
+                if(recorrido.info>0){ //forma ascendente >0
+                    int tmp;
+                    tmp=recorrido.info;
+                    recorrido.info=recorrido.info-1;
+                    recorrido.info=tmp;  
+                }
+            }
+        }
+        else{
+            System.out.println("No existe elementos en la lista");
+            }
+    }
+    //Borrar elemento de la lista
+    public void borrarElemento(int posicion){
+        if (posicion<=cantidadNodos()) {
+            if (posicion==1) {
+                raiz=raiz.sig;
+            }
+            else{
+                Nodo recorrido=raiz;
+                for (int i = 1; i <=posicion-2 ; i++) {
+                    recorrido=recorrido.sig;
+                }
+                Nodo proximo=recorrido.sig;
+                recorrido.sig=proximo.sig;
+            }
+        }
+        else{
+            System.out.println("La posicion"+posicion+"no existe");
+        }
+    }
+    public void intercambioElemento(int pos1,int pos2){
+        if (pos1<=cantidadNodos()&& pos2<=cantidadNodos()) {
+            Nodo recorrido1=raiz;
+            for (int i = 1; i < pos1; i++) {
+                recorrido1=recorrido1.sig;
+            }
+            Nodo recorrido2=raiz;
+             for (int i = 1; i < pos2; i++) {
+                recorrido2=recorrido2.sig;
+            }
+             int temp=recorrido1.info;
+             recorrido1.info=recorrido2.info;
+             recorrido2.info=temp;
+        }
+    }
 }
