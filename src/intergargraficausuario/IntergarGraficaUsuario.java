b4981d991ed9d921ea5dba2c69043c919f327d11
alumnos <alumnos@UIOOCC106-C-02.alumnosuio.ute.edu.ec>
2022-07-14 10:55:49 -0500

    Interfaz Grafica de Usuario

---
+++ src/intergargraficausuario/IntergarGraficaUsuario.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package intergargraficausuario;
+
+/**
+ *
+ * @author alumnos
+ */
+public class IntergarGraficaUsuario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //GUI --> IntergarGraficaUsuario
+        //IUX
+        //librerias AWT o Swing intergaz grafica
+        //modelo vista controlador
+        //paqueete se puede llevar de un proyecto a otro
+        //folder contenedor archivos externos 
+        //frmPrincipal formulario = new frmPrincipal();
+        //JDialog form se ejecuta al interior 
+        //MDI Application Sample Form principal de todo
+        new frmPrincipal().setVisible(true);   //para hacer visible al formulario
+    }
+    
+}
